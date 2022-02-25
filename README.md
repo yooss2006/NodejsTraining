d07ca0b69f9c54ad8c8f0686483ede621590dd76
yooss2006 <68742190+yooss2006@users.noreply.github.com>
2022-02-25 19:25:00 +0900

    git ignore add

--- README.md
+++
@@ -1,3 +0,0 @@
-# NodejsTraining
-
-codeLion의 일단 만드는 nodejs를 수강하며 실습한 내용을 저장합니다.
--- first-project/routes/template.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-router.get("/ejs", (req, res) => {
-  res.render("template", { data: "test data" });
-});
-
-module.exports = router;
